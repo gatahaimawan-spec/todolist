8eeebba026130384c139ca13b3525256ce517e67
gatahaimawan-spec <gata.haimawan@gmail.com>
2026-03-23 22:49:32 +0700

    todolisrt

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-function tutupdanfokus() {
-document.getElementById('check').checked = false;
-document.getElementById('todo-input').focus();
-if (checkbox){
-    checkbox.checked = false;
-}
-}
- if (InputTeks){
-    setTimeout(() => {
-        InputTeks.focus();
-    }, 300);
- }
+const inputan = document.querySelector("#todo-input");
+const tombol = document.querySelector("#task-btn");
+const wadah = document.querySelector("#task-list");
+
+console.log("cek!");
+console.log("cek:", wadah);
+
+tombol.addEventListener("click", function () {
+
+  //  TANGKI KOSONG
+  const pesanan = inputan.value;
+  // PENGISI TANGKI
+  const listBaru = document.createElement("li");
+  listBaru.textContent = pesanan;
+  //  RUANG MESIN YANG AKAN DIISI OLEH
+  // BAHAN BAKAR DARI TANGKI YANG DIHASILKAN
+  // OLEH PEGISI TANGKI
+  const tombolHapus = document.createElement("button");
+  tombolHapus.textContent = "X";
+  tombolHapus.classList.add("btn-hapus");
+  tombolHapus.addEventListener("click", function () {
+    event.stopPropagation();
+    listBaru.remove();
+  });
+  listBaru.appendChild(tombolHapus);
+  wadah.appendChild(listBaru);
+  // PENEGASAN BAHWA TANGKI KOSONG
+  inputan.value = "";
+  //   SAKLAR SELESAI
+  listBaru.addEventListener("click", function () {
+  listBaru.classList.toggle("selesai");
+});
+});
